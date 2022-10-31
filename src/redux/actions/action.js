fb3a072b68587b42324525485c1ca1e5a322dd4e
yadavdevendra <devendrayadav2022aug@cedcommerce.com>
2022-10-31 17:44:51 +0530

    redux thunk

--- src/redux/actions/action.js
+++ src/redux/actions/action.js
@@ -4,19 +4,11 @@ import axios from "axios";
 
 
 export const action = () => async (dispatch) => {
-  try {
+
     dispatch({ type: GET_DATA });
     const { data } = await axios.get(
       "https://jsonplaceholder.typicode.com/users"
     );
     dispatch({ type: GET_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({
-      type: GET_FAIL,
-      payload:
-        error.data && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    });
-  }
+    console.log(data);
 };
